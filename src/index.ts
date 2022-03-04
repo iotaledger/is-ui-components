72593b45a089659ac6cf9f132cb2fa9b49d77180
Ruben Alvarez <ralvarez@boxfish.studio>
2022-03-04 14:27:13 +0100

    Feat: Add Streams Manager component to lib

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,10 @@
 // Components
 import IdentityManager from './components/identity/identity-manager.svelte';
 import LoginRegister from './components/login-register/login-register.svelte';
+import StreamsManager from './components/streams/streams-manager.svelte';
 
 // Styles
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './scss/index.scss';
 
-export { IdentityManager, LoginRegister };
+export { IdentityManager, LoginRegister, StreamsManager };
