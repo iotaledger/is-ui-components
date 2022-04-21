033d7f31004314c1894509f7fdbf93e1a99461f7
Ruben Alvarez <ralvarez@boxfish.studio>
2022-04-21 18:26:51 +0200

    add jwt logic

--- src/lib/app/base.ts
+++ src/lib/app/base.ts
@@ -1,8 +1,10 @@
 import type { ClientConfig } from '@iota/is-client';
 import { ApiVersion, ChannelClient, IdentityClient } from '@iota/is-client';
-import { derived } from 'svelte/store';
+import { derived, get } from 'svelte/store';
 import { persistent } from './utils';
 import { NotificationType } from './types/notification';
+import { showNotification } from './notification';
+import { logout } from './identity';
 
 const config: ClientConfig = {
 	apiKey: import.meta.env.VITE_IOTA_IS_SDK_API_KEY as string,
@@ -33,4 +35,40 @@ authenticationData?.subscribe(($authenticationData) => {
 });
 
 
-console.log(NotificationType.Error)
+export const isJwtExpired = (token: string): boolean => {
+	try {
+		const expiry = (JSON.parse(window?.atob(token?.split('.')?.[1])))?.exp;
+		const now = new Date();
+
+		return now.getTime() > (expiry * 1000);
+	}
+	catch (error) {
+		showNotification({
+			type: NotificationType.Error,
+			message: 'Impossible to check JWT expiration.',
+		})
+		console.error("Failed to get JWT expiration status: ", error)
+	}
+}
+
+let jwtExpirationCheckInterval: NodeJS.Timeout;
+export function startPollExpirationCheckJWT(): void {
+	clearInterval(jwtExpirationCheckInterval)
+	jwtExpirationCheckInterval = setInterval(() => {
+		const jwt = get(authenticationData)?.jwt
+		if (jwt) {
+			const isJWTExpired = isJwtExpired(jwt)
+			if (isJWTExpired) {
+				logout()
+				showNotification({
+					type: NotificationType.Error,
+					message: 'JWT expired. Please login again.',
+				})
+			}
+		}
+	}, 60000);
+}
+
+export function stopPollExpirationCheckJWT(): void {
+	clearInterval(jwtExpirationCheckInterval)
+}
