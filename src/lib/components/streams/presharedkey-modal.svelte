545cac38005bd202afbf6b83ff9cc85855ac0d2e
Maqomed <maqomed.rahmanov@gmail.com>
2022-06-27 19:12:57 +0400

    Small modifications

--- src/lib/components/streams/presharedkey-modal.svelte
+++ src/lib/components/streams/presharedkey-modal.svelte
@@ -1,20 +1,23 @@
 <script lang="ts">
     import Modal from 'sveltestrap/src/Modal.svelte'
     import { Button, ModalBody, ModalFooter, ModalHeader } from 'sveltestrap'
+
     export let presharedKey
+
     let open = true;
     const toggle = () => (open = !open);
 </script>
+
 <div>
-    <Modal isOpen={open} {toggle} >
-      <ModalHeader {toggle}>Preshared Key</ModalHeader>
-      <ModalBody>
-        <h4>This key is not stored anywhere and can't be recovered.</h4>
-        <b>{presharedKey}</b>
-      </ModalBody>
-      <ModalFooter>
-        <Button color="secondary" on:click={toggle}>Cancel</Button>
-      </ModalFooter>
-    </Modal>
-  </div>
+  <Modal isOpen={open} {toggle}>
+    <ModalHeader {toggle}>Preshared Key</ModalHeader>
+    <ModalBody>
+      <h4>This key is not stored anywhere and can't be recovered.</h4>
+      <b><i>{presharedKey}</i></b>
+    </ModalBody>
+    <ModalFooter>
+      <Button color="secondary" on:click={toggle}>Cancel</Button>
+    </ModalFooter>
+  </Modal>
+</div>
 
