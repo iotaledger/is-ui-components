adcc926fdad08d40d5ed0b5523da137684b441c5
Maqomed <maqomed.rahmanov@gmail.com>
2022-06-29 15:11:28 +0400

    Warning style added. Input to copy the key is added

--- src/lib/components/streams/presharedkey-modal.svelte
+++ src/lib/components/streams/presharedkey-modal.svelte
@@ -1,22 +1,50 @@
 <script lang="ts">
     import Modal from 'sveltestrap/src/Modal.svelte'
-    import { Button, ModalBody, ModalFooter, ModalHeader } from 'sveltestrap'
+    import { Button, ModalBody, ModalFooter, Input, Label, Alert } from 'sveltestrap'
 
     export let presharedKey
+    let isEqual = false
 
-    let open = true;
-    const toggle = () => (open = !open);
+    let open = true
+    let color = 'warning'
+    let mouseClickOutsideModal
+    const toggle = () => {
+        if (mouseClickOutsideModal == false) {
+            open = !open
+        }
+    }
+    const handleInput = (e) => {
+        if (presharedKey == e.target.value) {
+            isEqual = true
+            mouseClickOutsideModal = false
+        } else {
+            isEqual = false
+        }
+    }
 </script>
 
 <div>
-  <Modal isOpen={open} {toggle}>
-    <ModalBody>
-      <h4>This key is not stored anywhere and can't be recovered.</h4>
-      Preshared Key: <b>{presharedKey}</b>
-    </ModalBody>
-    <ModalFooter>
-      <Button color="secondary" on:click={toggle}>Cancel</Button>
-    </ModalFooter>
-  </Modal>
-</div>
+    <Modal isOpen={open} {toggle}>
+        <ModalBody>
+            <Alert color="warning">
+                <h4 class="alert-heading text-capitalize">{color}</h4>
+                This key is not stored anywhere and can't be recovered.
+            </Alert>
 
+            Preshared Key: <b>{presharedKey}</b>
+            <div class="pt-2">
+                <Input
+                    type="text"
+                    name="text"
+                    id="presharedKeyText"
+                    placeholder="Copy preshared key here"
+                    on:input={handleInput}
+                />
+                <div />
+            </div></ModalBody
+        >
+        <ModalFooter>
+            <Button color="primary" on:click={toggle} disabled={!isEqual}>OK</Button>
+        </ModalFooter>
+    </Modal>
+</div>
