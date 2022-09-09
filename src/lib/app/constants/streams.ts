15b79f1c5ba3809be9806827fe1ae345f046dd8e
Juri Bogatyrjow <juri.bogatyrjow@gmail.com>
2022-09-09 16:32:35 +0200

    added env var

--- src/lib/app/constants/streams.ts
+++ src/lib/app/constants/streams.ts
@@ -4,4 +4,4 @@ export const DEFAULT_MESSAGES_PAGE_SIZE = 5
 export const DEFAULT_AUTHOR_FILTER_STATE = true
 export const DEFAULT_SUBSCRIBED_FILTER_STATE = false
 export const DEFAULT_REQUESTED_SUBSCRIPTION_STATE = false
-export const NETWORK_EXPLORER = "https://explorer.iota.org/mainnet/message/"
+export const NETWORK_EXPLORER = import.meta.env.NETWORK_EXPLORER ?? 'https://explorer.iota.org/mainnet/message/'
